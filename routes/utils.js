0fba1555a8b9d35a05bdc362f5fcb20789dcaf1c
lad475 <lad475@nyu.edu>
2018-12-04 09:39:01 -0500

    Fix eslint issues and syntax errors

--- routes/utils.js
+++ routes/utils.js
@@ -1,76 +1,61 @@
 const express = require('express');
+
 const router = express.Router();
 
 
 router.get('/feed', (req, res, next) => {
-
+  console.log(req, res, next);
 });
 
 router.get('/search', (req, res, next) => {
-	const results = [];
+  let results = [];
+  console.log(res, next);
 
-	/*
-	* take in query parameters: item name, zip, username, first name, last name
-	*/
-	if(req.query.itemName){
-		//search for item name with given string
-		if(results.length <= 0){
-			//get from database
-		}
-		else{
-			//filter by results
-			results = results.filter((val) => {
-				return val.itemName.toLowerCase().includes(req.query.itemName.toLowerCase());
-			}
-		}
-	}
+  // take in query parameters: item name, zip, username, name
 
-	if(req.query.zipcode){
-		if(results.length <= 0){
-			//get from database
-		}
-		else{
-			//filter by results
-      results = results.filter((val) => {
-        return val.zipcode.equals(req.query.zipcode);
-      }
-		}
-	}
+  if (req.query.itemName) {
+    // search for item name with given string
+    if (results.length <= 0) {
+      // get from database
+    } else {
+      // filter by results
+      results = results.filter(val => val.itemName.toLowerCase()
+        .includes(req.query.itemName.toLowerCase()));
+    }
+  }
 
-	if(req.query.username){
-		if(results.length <= 0){
-			//get from database
-		}
-		else{
-			//filter by results
-      results = results.filter((val) => {
-        return val.username.toLowerCase().includes(req.query.username.toLowerCase());
-      }
-		}
-	}
+  if (req.query.zipcode) {
+    if (results.length <= 0) {
+      // get from database
+    } else {
+      // filter by results
+      results = results.filter(val => val.zipcode.equals(req.query.zipcode));
+    }
+  }
 
-	if(req.query.firstName){
-		if(results.length <= 0){
-			//get from database
-		}
-		else{
-			//filter by results
-      results = results.filter((val) => {
-        return val.firstName.toLowerCase().includes(req.query.firstName.toLowerCase());
-      }
-		}
-	}
+  if (req.query.username) {
+    if (results.length <= 0) {
+      // get from database
+    } else {
+      // filter by results
+      results = results.filter(val => val.username.toLowerCase()
+        .includes(req.query.username.toLowerCase()));
+    }
+  }
 
-	if(req.query.lastName){
-		if(results.length <= 0){
-			//get from database
-		}
-		else{
-			//filter by results
-      results = results.filter((val) => {
-        return val.lastName.toLowerCase().includes(req.query.lastName.toLowerCase()); 
-      }
-		}
-	}
-
+  if (req.query.name) {
+    if (results.length <= 0) {
+      // get from database
+    } else {
+      // filter by results
+      results = results.filter(val => (val.firstName.toLowerCase()
+        .includes(req.query.name.toLowerCase())
+          || val.lastName.toLowerCase()
+            .includes(req.query.name.toLowerCase())
+          || (req.query.name.toLowerCase()
+            .includes(val.lastName.toLowerCase())
+            && req.query.name.toLowerCase()
+              .includes(val.firstName.toLowerCase()))));
+    }
+  }
 });
