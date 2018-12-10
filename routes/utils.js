de4670783b78c260c38d53af67a76551f9628eb0
Dan McKenna <d.mckenna24@gmail.com>
2018-12-10 14:35:22 -0500

    search implemented

--- routes/utils.js
+++ routes/utils.js
@@ -30,35 +30,57 @@ router.get('/search', (req, res, next) => {
     // search for item name with given string
     if (results.length <= 0) {
       // get from database
+      let ids = conHelper.getObjectIdByItemName(req.query.itemName);
+      for(let i = 0; i < ids.length; i++){
+        results[i] = conHelper.getObjectById(ids[i]);
+      }
+      return res.status(200).json(results);
+
     } else {
       // filter by results
       results = results.filter(val => val.itemName.toLowerCase()
         .includes(req.query.itemName.toLowerCase()));
+      return res.status(200).json(results);
     }
   }
 
   if (req.query.zipcode) {
     if (results.length <= 0) {
       // get from database
+      let ids = conHelper.getObjectIdByZipcode(req.query.zipcode);
+      for(let i = 0; i < ids.length; i++){
+        results[i] = conHelper.getObjectById(ids[i]);
+      }
+      return res.status(200).json(results);
     } else {
       // filter by results
       results = results.filter(val => val.zipcode.equals(req.query.zipcode));
+      return res.status(200).json(results);
     }
   }
 
   if (req.query.username) {
     if (results.length <= 0) {
       // get from database
+      results = conHelper.getUserListed(req.query.username);
+      return res.status(200).json(results);
     } else {
       // filter by results
       results = results.filter(val => val.username.toLowerCase()
         .includes(req.query.username.toLowerCase()));
+      return res.status(200).json(results);
     }
   }
 
   if (req.query.name) {
     if (results.length <= 0) {
       // get from database
+      let ids = conHelper.getObjectIdByName(req.query.name);
+      for(let i = 0; i < ids.length; i++){
+        results[i] = conHelper.getObjectById(ids[i]);
+      }
+      return res.status(200).json(results);
+
     } else {
       // filter by results
       results = results.filter(val => (val.firstName.toLowerCase()
@@ -69,6 +91,7 @@ router.get('/search', (req, res, next) => {
             .includes(val.lastName.toLowerCase())
             && req.query.name.toLowerCase()
               .includes(val.firstName.toLowerCase()))));
+      return res.status(200).json(results);
     }
   }
 });
