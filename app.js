eeac72b3d85a57dce2bcee69e2b72309721b6499
lad475 <lad475@nyu.edu>
2018-12-11 14:54:35 -0500

    Fixes gitignore and app file

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const utilsRouter = require('./routes/utils');
 const itemRouter = require('./routes/item');
-const mapReduce = require('./routes/mapRedReviews');
+const mapReduce = require('./routes/mapReduce');
 
 const setHeaders = require('./middleware/setHeaders');
 
@@ -24,6 +24,6 @@ app.use('/', indexRouter);
 app.use('/utils', utilsRouter);
 app.use('/item', itemRouter);
 app.use('/users', usersRouter);
-app.use('/mr', mapReduce);
+app.use('/reviews', mapReduce);
 
 module.exports = app;
